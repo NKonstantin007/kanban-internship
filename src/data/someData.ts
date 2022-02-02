f6705f278a018dcd1ebae298a2af50da913ae210
NKonstantin007 <kostya.1998.nosarev@mai.ru>
2022-02-02 09:24:24 +0700

    Delete examples

--- src/data/someData.ts
+++
@@ -1,9 +0,0 @@
-import { SomeEntity } from '../types/someEntity';
-
-// Shared api services
-export function getSomeData(): SomeEntity {
-  return {
-    a: 'a',
-    b: 'b',
-  };
-}
--- src/queries/useSomethingQueries.ts
+++
@@ -1,7 +0,0 @@
-import { useMemo } from 'react';
-
-export const useSomethingQuery = () => {
-  return useMemo(() => {
-    return 'Shared hooks which invoke hooks from react-query and can be used in all modules';
-  }, []);
-};
