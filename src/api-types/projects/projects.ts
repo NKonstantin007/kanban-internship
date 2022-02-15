0f455b5aa8b237659c3118537faab180c5c6a5d6
VKhayretdinov <vladislav.khayretdinov@gmail.com>
2022-02-15 12:00:50 +0700

    update eslint rules

--- src/api-types/projects/projects.ts
+++ src/api-types/projects/projects.ts
@@ -1,14 +1,12 @@
 import { Project } from '@/types/projects';
 
 export type ProjectApiModel = {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   _id: string;
   name: string;
   description: string;
 };
 
 const convertToProject = (projectApiModel: ProjectApiModel): Project => {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   const { _id, ...rest } = projectApiModel;
 
   return {
