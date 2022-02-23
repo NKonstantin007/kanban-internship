9ec29f4a3b8f9555d65f3d930273c4bd0c17288f
Neoclassic-alt <boris220-2011@mail.ru>
2022-02-23 13:06:59 +0700

    Delete user mocks

--- src/data/user/__mocks/userServicesMocks.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '@/types/user';
-import { withDelay } from '@/utils/time';
-
-export const getCurrentUserMock = withDelay((): User => {
-  return {
-    name: 'Alex',
-  };
-}, 1000);
